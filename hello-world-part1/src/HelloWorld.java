a0c92d15f7f5d1c28367ff473abc45d2165564cc
Stephen West <stevewestinlondon@gmail.com>
2023-08-26 08:22:37 +0100

    Part 1
    Simple Hello World Program

---
+++ hello-world-part1/src/HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * Adding the classic Hello World program
+ */
+public class HelloWorld {
+
+    // In IntelliJ we can type 'psvm' to auto generate a 'public static void main...'
+    public static void main(String[] args) {
+
+        // In IntelliJ, we can type 'sout' to auto generate 'System.out.println(..'
+        System.out.println("Hello World Part 1");
+    }
+}
