bca3b3431261b743295534bb89ffa9416358a4e6
stealthness <stevewestinlondon@gmail.com>
2025-03-04 19:28:42 +0000

    added basic java swing

---
+++ Swing/src/Main.java
@@ -0,0 +1,20 @@
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.SwingUtilities;
+
+public class Main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            JFrame frame = new JFrame("Basic Swing Application");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setSize(300, 200);
+
+            JButton button = new JButton("Click Me");
+            button.addActionListener(e -> JOptionPane.showMessageDialog(frame, "Button Clicked!"));
+
+            frame.getContentPane().add(button);
+            frame.setVisible(true);
+        });
+    }
+}
